93dabe75381b7f8f009ffe1bd1ac7b6653894cdd
rutul <rutulsuthar2018@gmail.com>
2023-09-18 12:48:15 +0530

    Practical 6 done

--- app/src/main/java/com/example/mad_practical_6_21012011129/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_6_21012011129/MainActivity.kt
@@ -4,19 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val Play : Button=findViewById(R.id.play_btn)
+        val Play:FloatingActionButton=findViewById(R.id.play_btn)
 
         Play.setOnClickListener {
             PlayPause()
         }
-        val Pause : Button=findViewById(R.id.stop_btn)
+        val Pause : FloatingActionButton=findViewById(R.id.stop_btn)
 
         Pause.setOnClickListener {
             Stop()
